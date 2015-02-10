1804b18cdbb107ff4b938de542e5182ebd14a18c
Embarc <brian.frohlich@hexagonmetrology.com>
2015-02-10 11:21:59 -0800

    Added wait gif for status page

--- js/status.js
+++ js/status.js
@@ -1,40 +1,44 @@
+$(document).ready(function()
+		{
+			$("#StatusContainer").css("display", "none");
+			$("#spinner").css("display", "block");
+			var hasArmData = 0;
+			var hasVersionData = 0;
 
+			var socket = io.connect();
+			socket.emit('client_data', Update);
+			socket.on('ARM', function(data)
+			{
+				$("#versionInput").val(data.Version);
+				$("#axesInput").val(data.Axes);
+				$("#volumeInput").val(data.ArmVolume);
+				$("#hwVersionInput").val(data.HWVersion);
+				$("#swInputHi").val(data.SWVersionHi + '.' + data.SWVersionLo);
+				var per = (data.Battery/255)*100
 
-    var socket = io.connect();
+				$("#battryPercentageId").html(Math.floor(per)+"%");
+				$("#batteryInput").val(" "+Math.floor(per)+"%")
 
-
-    $(document).ready(function() {
-
-         GetSettings();
-    });	
-
- function GetSettings()
-  {
-    	socket.emit('client_data', Update);
-  }
-
-  socket.on('ARM', function(data){
-  
-    $("#versionInput").val(data.Version);
-    $("#axesInput").val(data.Axes);
-    $("#volumeInput").val(data.ArmVolume);
-    $("#hwVersionInput").val(data.HWVersion);
-    $("#swInputHi").val(data.SWVersionHi);
-    $("#swInputLo").val(data.SWVersionLo);
-    //$("#batteryInput").val(data.Battery)
-     
-       var per = (data.Battery/255)*100
-   
-     $("#battryPercentageId").html(Math.floor(per)+"%");
-     $("#batteryInput").val(" "+Math.floor(per)+"%")
-
-   
-     $(".batteryPer .battery em font").css("width",Math.floor(per)+"%");
-    if(data.Ambient == 255)
-      $("#temperatureInput").val("N/A")
-	else	
-     $("#temperatureInput").val(data.Ambient+" Celsius")
-
-
-
-  });
+				$(".batteryPer .battery em font").css("width",Math.floor(per)+"%");
+				if(data.Ambient == 255)
+				$("#temperatureInput").val("N/A")
+				else
+				$("#temperatureInput").val(data.Ambient+" Celsius")
+				if(hasVersionData == 1)
+				{
+				$("#spinner").css("display", "none");
+				$("#StatusContainer").css("display", "block");
+				}
+				hasArmData = 1;
+			});
+			socket.emit('requestLoggingData', "/home/version.txt");
+			socket.on('LoggingDataDispatch', function(data)
+			{
+				$("#swInputLo").val(data[0] + '.' + data[2] + '.' +data[4]);
+				if(hasArmData == 1)
+				{
+				$("#spinner").css("display", "none");
+				$("#StatusContainer").css("display", "block");
+				}
+				hasVersionData = 1;			});
+		});
